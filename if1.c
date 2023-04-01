561045cd838cd417b82e8edd3d3699f0a687b639
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-04-01 19:34:55 +0530

    program

---
+++ if1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	printf("\nIf Start");
+	if(10==10)
+	{
+		printf("\nHello Everyone");
+		printf("\n Bye");
+	}
+	
+	printf("\nIf Ended");
+	getch();
+}
---
+++ runtimeError.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a,b=5;
+	int c;
+	c=a/b;
+	printf("\n value of c : %d",c);
+	getch();
+}
