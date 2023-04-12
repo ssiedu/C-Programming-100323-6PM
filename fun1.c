b8a4236034ec34680e078d9cda18322e96ee046f
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-04-12 19:55:36 +0530

    program

---
+++ fun1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void Hello();//function declaration
+void Hii();
+void main()
+{
+
+	Hello();//function call	
+	getch();
+}
+
+void Hello()
+{
+	
+	printf("\n Welcome to ssi");
+	printf("\n Hello EveryOne");
+	printf("\n This is a c class");
+	printf("\n Thank you");	
+	Hii();
+}
+void Hii()
+{
+	printf("\n HII");
+	printf("\n BYE");
+
+}
+
