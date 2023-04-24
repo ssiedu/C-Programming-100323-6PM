0c37b2acc8d16700988143c1825456d7e52aa634
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-04-24 19:57:36 +0530

    program

---
+++ malloc.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int n,i,*ptr,sum=0;
+	printf("\n Enter size of array : ");
+	scanf("%d",&n);
+	ptr=(int*)malloc(n*sizeof(int));
+	if(ptr==NULL)
+	{
+		printf("\n not sufficient Memory");
+		exit(0);
+	}
+	
+	printf("\n Enter Elements of array : ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",(ptr+i));
+		sum=sum+ *(ptr+i);
+	}
+	printf("\n sum of array elements : %d",sum);
+	free(ptr);
+	getch();
+}
